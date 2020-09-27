54bf03762508289f4e78770084a574ca6588adeb
senRsl <dong_M@yeah.net>
2020-09-27 15:57:41 +0800

    init

---
+++ spring_boot2/src/test/java/dc/test/spring/boot2/SpringBoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dc.test.spring.boot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
